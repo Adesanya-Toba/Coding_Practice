65c0f519a60ce3f7e68428e7cea522e7887285ba
Adesanya Toba <adesanyatoba@gmail.com>
2021-05-12 21:08:35 +0100

    linkedlist1.c

---
+++ LinkedList1.c
@@ -0,0 +1,55 @@
+/* Inserting at the beginning of the List */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct 
+{
+	int data;
+	struct Node* next; //C-style
+}Node;
+
+
+void Insert(Node** pointerHead, int x) //passing Node by refrence
+{
+	Node* temp = (Node*)malloc(sizeof(Node));
+	temp->data = x;
+	temp->next = *pointerHead;
+	*pointerHead = temp;
+}
+
+void Print(Node* head)
+{
+	// Node* temp = head; // copy the address of head here, so we don't modify the reference
+	printf("List is: ");
+	while (head!= NULL)
+	{
+		printf("%d ", head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	Node* head; // head points to the Head node 
+
+	head = NULL; // set to NULL for now i.e points nowhere now
+	
+    printf("Address of NULL: %d\n", head);
+    printf("Address of pointer head itself: %d\n\n", &head);
+
+	printf("How many numbers?\n");
+	int n, x, d = 1;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		printf("Enter number (%d): \n", i == 0? d : ++d);
+		scanf("%d", &x);
+		Insert(&head, x);
+		Print(head);
+	}
+	printf("Size of node: %ld\n", sizeof(Node));
+    printf("Size of int: %ld\n", sizeof(int));
+}
---
+++ Pointers3.c
@@ -0,0 +1,47 @@
+/*
+ * Pointers3.c
+ *
+ *  Created on: Sep 9, 2020
+ *      Author: ADESANYA TOBA
+ */
+#include <stdio.h>
+
+//Pointer to pointer
+int main()
+{
+
+	int x = 5;
+	int* p = &x;
+	*p = 6;
+
+	int** q = &p;
+	int*** r = &q;
+
+	printf("Address held by p, which is address of int x: %d\n", p);
+	printf("Value held in address pointed by p: %d\n", *p);
+	printf("Address of pointer p: %d\n", &p);
+	printf("\n");
+
+	printf("Address held by q: %d\n", q);
+	printf("Value held in address pointed to by q: %d\n", *q);
+	printf("Final value held in address pointed to %d\n", **q);
+	printf("\n");
+
+	printf("Address of pointer q itself: %d\n", &q);
+	printf("Address held by r, which is the address of q: %d\n", r);
+	printf("Address stored in the address held by q: %d\n", *r);
+	printf("Address stored in that address: %d\n", **r);
+	printf("Value stored in that address: %d\n", ***r);
+
+	printf("\n");
+
+	***r = 10;
+	printf("x = %d\n", x);
+	**q = *p + 2;
+	printf("x = %d\n", x);
+
+    int a = 7;
+    int* head = &a;
+
+    printf("%d\n", *head); // equivalent to *(&a) which is a!
+}

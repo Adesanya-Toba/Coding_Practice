83ae51751276e69a8e1a3f4606b30826569e9ade
Toba <adesanyatoba@gmail.com>
2021-08-26 18:13:40 +0100

    more practice with structs

--- struct_practice.c
+++ struct_practice.c
@@ -19,20 +19,32 @@ point test;
 
 int main(void)
 {
-    point *help = (point*)malloc(sizeof(point));
+    // point *help = (point*)malloc(sizeof(point));
+    point *help;
+
     test.x = 30;
     test.y = 90;
-    help->x = 40;
-    help->y = 26;
+    // help->x = 40;
+    // help->y = 26;
 
-    uint8_t num;
-    scanf("%hhd",&num);
-    printf("Is this your number: %d\n", num);
+    // here, instead of allocating memory with malloc, 
+    //I just used the same memory used by test i.e. I gave help the address of test to use
+    help = &test; 
 
-    printf("%d\n", test.x);
-    printf("%d\n", help->x);
-    printf("%d\n", help->y);
-    printf("%d\n", test.y);
+    // uint8_t num;
+    // scanf("%hhd",&num);
+    // printf("Is this your number: %d\n", num);
 
-    free(help);
+    printf("test.x: %d\n", test.x);
+    printf("test.y: %d\n\n", test.y);
+
+    // Changing the value of test.y via the pointer help
+    printf("Changing the value of test.y via a pointer\n");
+    help->y  = 87;
+    printf("test.y: %d\n\n", test.y);
+
+    printf("help.x: %d\n", help->x);
+    printf("help.y: %d\n", help->y);
+
+    // free(help);
 }

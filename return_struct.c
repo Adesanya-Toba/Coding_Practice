554c6c5d0d3885299a7e0660b8d0f00eebc8e79d
Toba <adesanyatoba@gmail.com>
2021-10-19 16:09:30 +0100

    retrun_struct, but through a pointer. As it should be!

--- return_struct.c
+++ return_struct.c
@@ -3,30 +3,34 @@
  * and also how to return NULL when the function fails.
 */
 #include <stdio.h>
-#include "ctype.h"
+#include <stdbool.h>
 
 typedef struct {
     int x;
     char text[10];
 }my_struct;
 
-my_struct return_struct (int a)
+int return_struct (int a, my_struct *test)
 {
-    my_struct test;
-
-    test.x = a;
-    if (test.x > 10){
-        return test;
+    test->x = a;
+    if (test->x > 10){
+        return 1;
     }
-    return;
+    return 0;
 }
 
 int main(void)
 {
     my_struct main_struct;
+
     //printf("my_sturct size: %lu\n ", sizeof(my_struct));
 
-    main_struct = return_struct(4);
+    int ret = return_struct(13, &main_struct.x);
     printf("main_struct.x = %d\n", main_struct.x);
+    printf("Return = %d\n", ret);
+
+    // Does the bool type exist in C? Yes, in "stdbool.h"
+    bool hello = false;
+    printf("Boolean: %d", hello);
 }
 

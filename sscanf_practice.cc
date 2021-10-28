25b678c5069eacbb42ecafca54ad00a39c8633b6
Toba <adesanyatoba@gmail.com>
2021-10-28 16:50:43 +0100

    More sscanf practice

--- sscanf_practice.cc
+++ sscanf_practice.cc
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-    char *name1 = "123Hello";
+    const char *name1 = "Hello%d";
     char name2[20];
     int num, res;
     
@@ -18,7 +18,11 @@ int main()
     cout << "Result of sscanf = " << res << endl;
     printf("this is the timestamp = %d\n", num);
     // printf("this is name2 = %s\n", name2);
-
+    
+    // Can sscanf take variable names as the format argument? Yes, it can!
+    sscanf("Hello1009", name1, &num);
+    printf("this is the extracted num = %d\n", num);
+    printf(name1, num); // Very impressive, hmmm
 
     return 0;
 }
